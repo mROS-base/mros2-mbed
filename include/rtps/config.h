0b0cd4d8d1579da3dde929f1df8495b8ac24ac82
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-10-04 19:07:25 +0900

    restore parameter from mros2-ready version for embeddedRTPS

--- include/rtps/config.h
+++ include/rtps/config.h
@@ -46,11 +46,11 @@ const GuidPrefix_t BASE_GUID_PREFIX{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12};
 const uint8_t DOMAIN_ID = 0; // 230 possible with UDP
 const uint8_t NUM_STATELESS_WRITERS = 2;
 const uint8_t NUM_STATELESS_READERS = 2;
-const uint8_t NUM_STATEFUL_READERS = 2;
-const uint8_t NUM_STATEFUL_WRITERS = 2;
+const uint8_t NUM_STATEFUL_READERS = 8;
+const uint8_t NUM_STATEFUL_WRITERS = 8;
 const uint8_t MAX_NUM_PARTICIPANTS = 1;
-const uint8_t NUM_WRITERS_PER_PARTICIPANT = 4;
-const uint8_t NUM_READERS_PER_PARTICIPANT = 4;
+const uint8_t NUM_WRITERS_PER_PARTICIPANT = 16;
+const uint8_t NUM_READERS_PER_PARTICIPANT = 16;
 const uint8_t NUM_WRITER_PROXIES_PER_READER = 3;
 const uint8_t NUM_READER_PROXIES_PER_WRITER = 3;
 
@@ -63,21 +63,21 @@ const uint8_t MAX_NUM_READER_CALLBACKS = 5;
 const uint8_t HISTORY_SIZE_STATELESS = 2;
 const uint8_t HISTORY_SIZE_STATEFUL = 10;
 
-const uint8_t MAX_TYPENAME_LENGTH = 20;
-const uint8_t MAX_TOPICNAME_LENGTH = 20;
+const uint8_t MAX_TYPENAME_LENGTH = 40;
+const uint8_t MAX_TOPICNAME_LENGTH = 40;
 
-const int HEARTBEAT_STACKSIZE = 1200;          // byte
-const int THREAD_POOL_WRITER_STACKSIZE = 1100; // byte
-const int THREAD_POOL_READER_STACKSIZE = 1600; // byte
-const uint16_t SPDP_WRITER_STACKSIZE = 550;    // byte
+const int HEARTBEAT_STACKSIZE = 4096;          // byte
+const int THREAD_POOL_WRITER_STACKSIZE = 4096; // byte
+const int THREAD_POOL_READER_STACKSIZE = 4096; // byte
+const uint16_t SPDP_WRITER_STACKSIZE = 4096;    // byte
 
 const uint16_t SF_WRITER_HB_PERIOD_MS = 4000;
-const uint16_t SPDP_RESEND_PERIOD_MS = 1000;
+const uint16_t SPDP_RESEND_PERIOD_MS = 10000;
 const uint8_t SPDP_CYCLECOUNT_HEARTBEAT =
     2; // skip x SPDP rounds before checking liveliness
 const uint8_t SPDP_WRITER_PRIO = 24;
 const uint8_t SPDP_MAX_NUMBER_FOUND_PARTICIPANTS = 5;
-const uint8_t SPDP_MAX_NUM_LOCATORS = 1;
+const uint8_t SPDP_MAX_NUM_LOCATORS = 5;
 const Duration_t SPDP_DEFAULT_REMOTE_LEASE_DURATION = {
     100, 0}; // Default lease duration for remote participants, usually
              // overwritten by remote info
@@ -93,7 +93,7 @@ const int THREAD_POOL_NUM_WRITERS = 1;
 const int THREAD_POOL_NUM_READERS = 1;
 const int THREAD_POOL_WRITER_PRIO = 24;
 const int THREAD_POOL_READER_PRIO = 24;
-const int THREAD_POOL_WORKLOAD_QUEUE_LENGTH = 10;
+const int THREAD_POOL_WORKLOAD_QUEUE_LENGTH = 20;
 
 constexpr int OVERALL_HEAP_SIZE =
     THREAD_POOL_NUM_WRITERS * THREAD_POOL_WRITER_STACKSIZE +
