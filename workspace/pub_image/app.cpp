959bfa5e3db2cbf669a64aec3df80c73362cbb05
smoriemb <USERNAME@users.noreply.github.com>
2023-01-01 10:43:39 +0900

    Prototype fragmented message support.

---
+++ workspace/pub_image/app.cpp
@@ -0,0 +1,71 @@
+/* mros2 example
+ * Copyright (c) 2022 smorita_emb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mbed.h"
+#include "mros2.h"
+#include "sensor_msgs/msg/image.hpp"
+#include "EthernetInterface.h"
+
+#include "mros_image.h"
+
+#define IP_ADDRESS ("192.168.11.2") /* IP address */
+#define SUBNET_MASK ("255.255.255.0") /* Subnet mask */
+#define DEFAULT_GATEWAY ("192.168.11.1") /* Default gateway */
+
+
+int main(int argc, char* argv[])
+{
+  EthernetInterface network;
+  network.set_dhcp(false);
+  network.set_network(IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY);
+  nsapi_size_or_error_t result = network.connect();
+
+  printf("mros2-posix start!\r\n");
+  printf("app name: pub_image\r\n");
+  mros2::init(0, NULL);
+  MROS2_DEBUG("mROS 2 initialization is completed\r\n");
+  
+  mros2::Node node = mros2::Node::create_node("mros2_node");
+  mros2::Publisher pub = node.create_publisher<sensor_msgs::msg::Image>("to_linux", 10);
+
+  osDelay(100);
+  MROS2_INFO("ready to pub image\r\n");
+
+  auto msg = sensor_msgs::msg::Image();
+
+  while (1) {
+   msg.sec = time(NULL);
+   msg.nanosec = 0;
+   msg.frame_id = "frame";
+   msg.height = MROS_IMAGE_HEIGHT;
+   msg.width = MROS_IMAGE_WIDTH;
+   msg.encoding = "rgb8";
+   msg.is_bigendian = 0;
+   msg.step = MROS_IMAGE_WIDTH * 3;
+   size_t image_size = sizeof(mros_image);
+   msg.data.resize(image_size);
+   std::memcpy(reinterpret_cast<char *>(&msg.data[0]),
+	       mros_image, image_size);    
+
+   printf("publishing image\r\n");
+   pub.publish(msg);
+
+   osDelay(1000);
+  }
+
+  mros2::spin();
+  return 0;
+}
---
+++ workspace/pub_long_string_sub_crc/templates.hpp
@@ -0,0 +1,12 @@
+
+#include "std_msgs/msg/u_int32.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+template mros2::Publisher mros2::Node::create_publisher<std_msgs::msg::String>(std::string topic_name, int qos);
+template void mros2::Publisher::publish(std_msgs::msg::String &msg);
+
+
+
+template mros2::Subscriber mros2::Node::create_subscription(std::string topic_name, int qos, void (*fp)(std_msgs::msg::UInt32*));
+template void mros2::Subscriber::callback_handler<std_msgs::msg::UInt32>(void *callee, const rtps::ReaderCacheChange &cacheChange);
